fba476df454fbd3fb5458b85e6e12802ffb7a8e8
Mike Fulcher <michael.fulcher@gmail.com>
2022-10-21 14:44:03 +1300

    Initial customisation for ceiling fan accessory

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,14 +1,15 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { Service, PlatformAccessory, CharacteristicValue } from "homebridge"
 
-import { ExampleHomebridgePlatform } from './platform';
+import { CeilingFanPlatform } from "./platform"
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
+export class CeilingFanAccessory {
+  private fanService: Service
+  private lightService: Service
 
   /**
    * These are just used to create a working example
@@ -17,125 +18,141 @@ export class ExamplePlatformAccessory {
   private exampleStates = {
     On: false,
     Brightness: 100,
-  };
+  }
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: CeilingFanPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, "Brilliant")
+      .setCharacteristic(this.platform.Characteristic.Model, "WiFi DC Ceiling Fan")
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, "20918")
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.fanService = this.accessory.getService(this.platform.Service.Fan) ||
+      this.accessory.addService(this.platform.Service.Fan)
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.lightService = this.accessory.getService(this.platform.Service.Lightbulb) ||
+      this.accessory.addService(this.platform.Service.Lightbulb)
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    this.fanService.setCharacteristic(this.platform.Characteristic.Name, "Ceiling Fan")
+    this.lightService.setCharacteristic(this.platform.Characteristic.Name, "Ceiling Fan Light")
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.fanService.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setFanOn.bind(this))
+      .onGet(this.getFanOn.bind(this))
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    this.fanService.getCharacteristic(this.platform.Characteristic.RotationDirection)
+      .onSet(this.setFanRotationDirection.bind(this))
+      .onGet(this.getFanRotationDirection.bind(this))
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+    this.fanService.getCharacteristic(this.platform.Characteristic.RotationSpeed)
+      .setProps({ minStep: 20 })
+      .onSet(this.setFanRotationSpeed.bind(this))
+      .onGet(this.getFanRotationSpeed.bind(this))
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    this.lightService.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setLightOn.bind(this))
+      .onGet(this.getLightOn.bind(this))
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    // /**
+    //  * Updating characteristics values asynchronously.
+    //  *
+    //  * Example showing how to update the state of a Characteristic asynchronously instead
+    //  * of using the `on("get")` handlers.
+    //  * Here we change update the motion sensor trigger states on and off every 10 seconds
+    //  * the `updateCharacteristic` method.
+    //  *
+    //  */
+    // let motionDetected = false
+    // setInterval(() => {
+    //   // EXAMPLE - inverse the trigger
+    //   motionDetected = !motionDetected
 
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+    //   // push the new value to HomeKit
+    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected)
+    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected)
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    //   this.platform.log.debug("Triggering motionSensorOneService:", motionDetected)
+    //   this.platform.log.debug("Triggering motionSensorTwoService:", !motionDetected)
+    // }, 10000)
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
+  async setFanOn(value: CharacteristicValue) {
     // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    // this.exampleStates.On = value as boolean
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    this.platform.log.debug("Set Fan Characteristic On ->", value)
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
+  async getFanOn(): Promise<CharacteristicValue> {
+    // const isOn = this.exampleStates.On
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    // this.platform.log.debug("Get Fan Characteristic On ->", isOn)
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    // // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE)
 
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+    // return isOn
+    return false
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async setFanRotationDirection(value: CharacteristicValue) {
+    // implement your own code to turn your device on/off
+    // this.exampleStates.On = value as boolean
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    this.platform.log.debug("Set Fan Characteristic Rotation Direction ->", value)
+  }
+
+  async getFanRotationDirection(): Promise<CharacteristicValue> {
+    // const isOn = this.exampleStates.On
+
+    // this.platform.log.debug("Get Fan Characteristic On ->", isOn)
+
+    // // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE)
+
+    // return isOn
+    return false
+  }
+
+  async setFanRotationSpeed(value: CharacteristicValue) {
+    // implement your own code to turn your device on/off
+    // this.exampleStates.On = value as boolean
+
+    this.platform.log.debug("Set Fan Characteristic Rotation Speed ->", value)
+  }
+
+  async getFanRotationSpeed(): Promise<CharacteristicValue> {
+    // const isOn = this.exampleStates.On
+
+    // this.platform.log.debug("Get Fan Characteristic On ->", isOn)
+
+    // // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE)
+
+    // return isOn
+    return false
+  }
+
+  async setLightOn(value: CharacteristicValue) {
+    // implement your own code to turn your device on/off
+    // this.exampleStates.On = value as boolean
+
+    this.platform.log.debug("Set Lightbulb Characteristic On ->", value)
+  }
+
+  async getLightOn(): Promise<CharacteristicValue> {
+    // const isOn = this.exampleStates.On
+
+    // this.platform.log.debug("Get Fan Characteristic On ->", isOn)
+
+    // // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE)
+
+    // return isOn
+    return false
   }
 
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,2 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export const PLATFORM_NAME = 'CeilingFanPlatform';
-
-/**
- * This must match the name of your plugin as defined the package.json
- */
-export const PLUGIN_NAME = 'homebridge-ceiling-fan';
+export const PLATFORM_NAME = "CeilingFanPlatform"
+export const PLUGIN_NAME = "homebridge-ceiling-fan"
