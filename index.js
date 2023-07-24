49468caad806053c00d51369d4a69978e884facd
Mike Fulcher <michael.fulcher@gmail.com>
2023-07-25 09:43:43 +1200

    Need to define an alias

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import { PLATFORM_NAME } from "./settings.js"
 import CeilingFanPlatform from "./platform.js"
 
 export default (api) => {
-  api.registerPlatform(PLATFORM_NAME, CeilingFanPlatform)
+  // 2nd PLATFORM_NAME is the alias which is required for the schema to work
+  api.registerPlatform(PLATFORM_NAME, PLATFORM_NAME, CeilingFanPlatform)
 }
