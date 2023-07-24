35657433eb4745e5a8428424d2ade1484c4aa285
Mike Fulcher <michael.fulcher@gmail.com>
2023-07-25 10:33:01 +1200

    Use plugin name as alias

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { PLATFORM_NAME, PLATFORM_ALIAS } from "./settings.js"
+import { PLUGIN_NAME, PLATFORM_NAME } from "./settings.js"
 import CeilingFanPlatform from "./platform.js"
 
 export default (api) => {
-  api.registerPlatform(PLATFORM_NAME, PLATFORM_ALIAS, CeilingFanPlatform)
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, CeilingFanPlatform)
 }
--- settings.js
+++ settings.js
@@ -4,11 +4,6 @@
 export const PLATFORM_NAME = "TuyaCeilingFan"
 
 /**
- * This is the alias for the platform that is used in config.schema.json
- */
-export const PLATFORM_ALIAS = "tuya-ceiling-fan"
-
-/**
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = "homebridge-tuya-ceiling-fan"
